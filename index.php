0289f1997e6ed05bac4cdfa1cc46b01a7a19a79e
IanTuitoekk <iantuitoek4@gmail.com>
2024-09-11 16:14:27 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <!-- <head>Index</head> -->
+    <body>
+        <!-- <p>First Page</p> -->
+        <?php
+ 
+    require "load.php";
+    $ObjLayouts->heading();
+    $ObjMenus->main_menu();
+    $ObjHeadings->main_banner();
+    $ObjCont->about_content();
+    $ObjCont->side_bar();
+    $ObjLayouts->footer();
+    ?>
+
+    </body>
+    </html>
+    
+   
---
+++ load.php
@@ -0,0 +1,53 @@
+<?php
+
+//Class Auto Load
+
+function classAutoLoad($classname)
+{
+    $directories = ["contents", "layouts", "menus"];
+
+    foreach ($directories as $dir) {
+    
+        $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR
+            . $classname . ".php";
+        if (file_exists($filename) and is_readable($filename)) {
+            require_once $filename;
+        }
+    }
+}
+
+spl_autoload_register('classAutoLoad');
+
+//Create instances of all classes
+
+// require_once "layouts/layouts.php";
+$ObjLayouts = new layouts();
+// require_once "menus/menus.php";
+$ObjMenus = new menus();
+// require_once "contents/headings.php";
+$ObjHeadings = new headings();
+$ObjCont=new contents();
+
+// $arr = ["black", "white", "green", "red"];
+
+// foreach($arr AS $color){
+//    print $color . "<br>";
+// }
+
+// print dirname(_FILE_);
+// print "<br>";
+// print "<br>";
+// print $_SERVER["PHP_SELF"];
+// print "<br>";
+// print "<br>";
+// print basename($_SERVER["PHP_SELF"]);
+// print "<br>";
+// print "<br>";
+
+// if(file_exists("index.php")){
+//    print "yes";
+// }
+// else{
+//    print "no";
+// }
+?>
